f320b2a149df9244f4f3b29bd3ae6630c3306f18
Niklas <niklasberby@gmail.com>
2023-11-24 14:46:08 +0100

    some layout stuff

--- project/src/routes/+page.svelte
+++ project/src/routes/+page.svelte
@@ -1,1 +1,27 @@
-<p class="p-4 bg-gray-100 rounded-md">test lol</p>
+<script lang="ts">
+    let test = [1, 2, 3, 4, 5];
+</script>
+
+
+<div class="container mx-auto">
+    <!-- main content flex -->
+    <div class="flex flex-col p-4 lg:my-16 lg:mx-24 border rounded-lg">
+
+        <div class="flex flex-col lg:flex-row gap-8 w-full items-center">
+
+			<!-- logg inn -->
+			<a href="#">
+				<div class="rounded-lg shadow-lg hover:bg-slate-300">
+					<p class="p-4 text-xl">Logg inn</p>
+				</div>
+			</a>
+
+			<p class="text-3xl lg:text-5xl p-2">Reiseguide</p>
+
+        </div>
+
+        <div class="flex flex-col lg:flex-row gap-4">
+			
+        </div>
+    </div>
+</div>
