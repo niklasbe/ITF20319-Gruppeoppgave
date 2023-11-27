f185c839eb0823fe42f959f76fbe9b7a43008b2f
Niklas <niklasberby@gmail.com>
2023-11-27 15:30:00 +0100

    Refactor Tour.ts and remove unused index.ts

--- project/src/lib/Tour.ts
+++ project/src/lib/Tour.ts
@@ -1,41 +1,69 @@
 import { writable } from "svelte/store";
 import jsonData from "../tours.json";
 
-// create a typescript array of tours from the json data
+/**
+ * An array of Tour objects.
+ * This array is created by parsing the JSON data representing the tours.
+ */
 export const tours: Tour[] = jsonData;
 
-// store booked tours in a separate array
+/**
+ * An array of Tour objects that have been booked.
+ * Initially, this array is empty. As tours are booked, they are added to this array.
+ */
 export const bookedTours: Tour[] = [];
 
+
+/**
+ * Represents a tour.
+ */
 export interface Tour {
-    id: number;
-    date: string;
-    time: string;
-    location: string;
-    title: string;
-    description: string;
-    price: number;
-    spots: number;
-    spotsLeft: number;
-    guide: string;
+    id: number;             /* The unique identifier of the tour. */
+    date: string;           /* The date of the tour in ISO 8601 format (YYYY-MM-DD). */
+    time: string;           /* The time of the tour in 24-hour format (HH:MM). */
+    location: string;       /* The location where the tour will take place. */
+    title: string;          /* The title or name of the tour. */
+    description: string;    /* A brief description of what the tour entails. */
+    price: number;          /* The price of the tour per person. */
+    spots: number;          /* The total number of spots available for the tour. */
+    spotsLeft: number;      /* The number of spots left available for booking. */
+    guide: string;          /* The name of the guide leading the tour. */
 }
 
 
+/**
+ * Represents the status of a user in the system.
+ */
 export enum Status {
-    NotLoggedIn,
-    User,
-    Guide,
-    Admin
+    NotLoggedIn,  /* The user is not logged in. */
+    User,         /* The user is logged in as a regular user. */
+    Guide,        /* The user is logged in as a guide. */
+    Admin         /* The user is logged in as an admin. */
 }
 
-/* 
-    Returns a tour with the given id
-    If no tour is found, return undefined
-*/
+
+/**
+ * Gets a tour by its ID.
+ * 
+ * This function iterates over the existing tours and returns the first tour that matches the provided ID.
+ * If no tour matches the provided ID, it returns undefined.
+ * 
+ * @param {number} id - The ID of the tour to retrieve.
+ * @returns {Tour | undefined} The tour with the matching ID, or undefined if no tour matches the provided ID.
+ */
 export const getTourById = (id: number): Tour | undefined => {
     return tours.find(tour => tour.id === id);
 }
 
+/**
+ * Gets the first available ID for a new tour.
+ * 
+ * This function iterates over the existing tours and finds the highest ID.
+ * It then returns one more than the highest ID, which will be the first available ID for a new tour.
+ * If there are no existing tours, it returns 1.
+ * 
+ * @returns {number} The first available ID for a new tour.
+ */
 export const getFirstAvailableId = (): number => {
     let id = 0;
     if (tours) {
@@ -49,6 +77,14 @@ export const getFirstAvailableId = (): number => {
 }
 
 
+/**
+ * An array of strings representing the different login statuses a user can have.
+ * These statuses correspond to the values in the Status enum.
+ */
 export const statusStrings = ["NotLoggedIn", "User", "Guide", "Admin"];
 
+/**
+ * A writable store that holds the current login status of the user.
+ * It is initialized to "NotLoggedIn", which corresponds to Status.NotLoggedIn in the Status enum.
+ */
 export const loginStatus = writable(statusStrings[Status.NotLoggedIn]);
